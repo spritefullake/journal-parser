5382911b5a444b88289315f137282439cda9a298
spritefullake <42ahmad@gmail.com>
2020-03-17 12:14:13 -0400

    updated tests to reflect file changes

--- analyzer_test.py
+++ analyzer_test.py
@@ -4,9 +4,9 @@ from functools import reduce
 from itertools import chain
 
 class TestAnalyzer(unittest.TestCase):
-    journal_file = "Journal.json"
+    journal_file = "out/Journal.json"
     def test_unique_authors_same_count(self):
-        data = read_in_json("Journal.json")
+        data = read_in_json(journal_file)
         total_count = len(data)
         by_author = collect_unique(data,"author")
         flat = list(chain(*by_author.values()))
